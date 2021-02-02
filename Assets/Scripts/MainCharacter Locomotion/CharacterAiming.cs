42fdce3a2fb151a4dea3962baa5a97e3aed4dd03
Perf0rat0r <72764747+Perf0rat0r@users.noreply.github.com>
2021-02-02 23:21:54 +0200

    Add empty project

---
+++ Assets/Scripts/MainCharacter Locomotion/CharacterAiming.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public Cinemachine.AxisState xAxis;
+    public Cinemachine.AxisState yAxis;
+
+    public Transform cameraLookAt;
+
+    public float turnSpeed = 7.5f;
+
+    bool AltPressed;
+
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        xAxis.Update(Time.fixedDeltaTime);
+        yAxis.Update(Time.fixedDeltaTime);
+
+        cameraLookAt.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, 0);
+
+        if (!Input.GetKey(KeyCode.LeftAlt))
+        {
+            RotatingPlayerToCameraView();
+        }
+    }
+
+    void RotatingPlayerToCameraView()
+    {
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler( 0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/MainCharacter Locomotion/MainCharacterLocomotion.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterLocomotion : MonoBehaviour
+{
+    Animator animator;
+
+    float clampMax = .5f;
+    float clampMin = -.5f;
+    float runClampMax = 1f;
+    float runClampMin = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Running();
+        Jumping();
+    }
+
+    void Move()
+    {
+        if (animator.GetBool("isRunning"))
+        {
+            
+            animator.SetFloat("VerticalRunning", Mathf.Clamp(Input.GetAxis("Vertical"), 0, runClampMax));
+            animator.SetFloat("HorizontalRunning", Mathf.Clamp(Input.GetAxis("Horizontal"), runClampMin, runClampMax));
+            animator.SetFloat("Vertical", Mathf.Clamp(Input.GetAxis("Vertical"), -1, 1f));
+            animator.SetFloat("Horizontal", Mathf.Clamp(Input.GetAxis("Horizontal"), -1f, 1f));
+        }
+        else
+        {
+            animator.SetFloat("Vertical", Mathf.Clamp(Input.GetAxis("Vertical"), clampMin, clampMax));
+            animator.SetFloat("Horizontal", Mathf.Clamp(Input.GetAxis("Horizontal"), clampMin, clampMax));
+        }
+    }
+
+    void Running()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && animator.GetFloat("Vertical") >= .3f) 
+        {
+            animator.SetBool("isRunning", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift)) 
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void Jumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && animator.GetFloat("Vertical") == 0)
+        {
+            animator.SetTrigger("isIdleJumping");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && animator.GetFloat("Vertical") > .5f)
+        {
+            animator.SetTrigger("isJumping");
+        }
+    }
+}
