47eb1fb1ae54c130e85b09a232b52b89db0eda85
Perf0rat0r <72764747+Perf0rat0r@users.noreply.github.com>
2021-02-07 22:09:22 +0200

    Added cut-scene base

---
+++ Assets/Scripts/CutScenes/CutScene.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CutScene : MonoBehaviour
+{ 
+    public GameObject mainCharacter;
+    private MainCharacterLocomotion mainChrLoc;
+
+    public int textAreas;
+    public TextMeshProUGUI[] text = new TextMeshProUGUI[1];
+    [TextArea]
+    public string[] sentences;
+    public float typingSpeed;
+
+    private int index;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mainChrLoc = mainCharacter.GetComponent<MainCharacterLocomotion>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("EndCutScene");
+            animator.SetBool("SkipHint", false);
+            Destroy(gameObject, 2);
+        }
+    }
+
+   IEnumerator TypeEffect()
+   {
+      
+       foreach(char letter in sentences[index].ToCharArray())
+       {
+           text[index].text += letter;
+           yield return new WaitForSeconds(typingSpeed);
+       }
+           index++;
+           animator.SetTrigger("NextImage");
+           if (index >= textAreas)
+           {
+               animator.SetBool("SkipHint", true);
+           }
+   }
+
+    void endCutScene()
+    {
+        animator.SetTrigger("EndCutScene");
+    }
+
+    void StopTime()
+    {
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/CutScenes/CutSceneTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneTrigger : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject mainCharacter;
+    private MainCharacterLocomotion mainChrLoc;
+
+    void Start()
+    {
+        mainChrLoc = mainCharacter.GetComponent<MainCharacterLocomotion>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger("StartCutScene");
+    }
+}
